33df59b7c4143bbf29f4019c144c0db69b4a82b8
Jacob Haning <jacob.haning@cardinalhealth.net>
2020-02-20 08:07:48 -0600

    initial commit

---
+++ titanic_survival.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from IPython.display import display  # Allows the use of display() for DataFrames
+
+# Import supplementary visualizations code visuals.py
+from titanic_survival_exploration import visuals as vs
+
+# Pretty display for notebooks
+# %matplotlib inline
+
+# Load the dataset
+in_file = 'titanic_data.csv'
+full_data = pd.read_csv(in_file)
+
+# Print the first few entries of the RMS Titanic data
+display(full_data.head())
